539b1157cbe9481a28921c2a1168b2891b08f5c9
Ednelson Amaral <eadamaral@gmail.com>
2024-07-12 06:46:48 -0300

    Update header.js

--- header.js
+++ header.js
@@ -1,9 +1,20 @@
+// const menuicon = document.getElementById('menuicon');
+// const menumobile = document.getElementById('menumobile');
+
+// menuicon.addEventListener('click', function() {
+//   if (menumobile.style.display === 'none') {
+//     menumobile.style.display = 'block';
+//   } else {
+//     menumobile.style.display = 'none';
+//   }
+// });
+
 const menuicon = document.getElementById('menuicon');
 const menumobile = document.getElementById('menumobile');
 
 menuicon.addEventListener('click', function() {
-  if (menumobile.style.display === 'none') {
-    menumobile.style.display = 'block';
+  if (menumobile.style.display === 'block') {
+    menumobile.style.display = 'none';
   } else {
     menumobile.style.display = 'none';
   }
